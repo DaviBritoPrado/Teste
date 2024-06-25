9cc016f6a93bc7dff6a258423049a7b10aec63c9
João Victor <jvvictorls@hotmail.com>
2024-06-25 00:50:27 -0300

    Feat: Adicionei a conexão com o backend

--- Tela-login-main/script.js
+++ Tela-login-main/script.js
@@ -1,7 +1,19 @@
+const nome = document.getElementById('nome');
 const emailInput = document.getElementById('email');
 const passwordInput = document.getElementById('password');
-const telefoneInput = document.getElementById('telefone');
-const usuarioInput = document.getElementById('usuario');
+const cpf = document.getElementById('cpf');
+const endereco = document.getElementById('endereco');
+const interesse = document.getElementById('interesse');
+const form = document.getElementById('form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+})
+
+nome.addEventListener('change', function() {
+  const nomeValue = nome.value;
+  console.log(nomeValue);
+})
 
 emailInput.addEventListener('change', function() {
   const email = emailInput.value;
@@ -13,12 +25,42 @@ passwordInput.addEventListener('change', function() {
   console.log(password);
 })
 
-telefoneInput.addEventListener('change', function() {
-  const telefone = telefoneInput.value
-  console.log(telefone);
+cpf.addEventListener('change', function() {
+  const cpfValue = cpf.value
+  console.log(cpfValue);
 })
 
-usuarioInput.addEventListener('change', function() {
-  const usuario = usuarioInput.value
-  console.log(usuario);
+endereco.addEventListener('change', function() {
+  const enderecoValue = endereco.value
+  console.log(enderecoValue);
+})
+
+interesse.addEventListener('change', function() {
+  const interesseValue = interesse.value
+  console.log(interesseValue);
+})
+
+const sendBtn = document.getElementById('send-btn');
+
+sendBtn.addEventListener('click', async function() {
+  const result = await fetch('http://localhost:8001/signin', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      name: nome.value,
+      email: emailInput.value,
+      password: passwordInput.value,
+      cpf: cpf.value,
+      address: endereco.value,
+      interest: interesse.value
+    })
+  });
+  console.log(result)
+
+  const data = await result.json();
+
+  console.log(data);
+  console.log('clicou no botão')
 })
